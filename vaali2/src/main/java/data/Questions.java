9fef4afe2938b5f5631c7163ce6d26b6eb762600
JanetteLehto <75020018+JanetteLehto@users.noreply.github.com>
2022-04-29 12:30:54 +0300

    Update Questions.java

--- vaali2/src/main/java/data/Questions.java
+++ vaali2/src/main/java/data/Questions.java
@@ -10,39 +10,57 @@ import javax.persistence.NamedQuery;
 
 public class Questions {
 
-	/**
-	 * 
-	 */
+
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int question_id;
 	private String question;
+	
+	
 
 	public Questions() {
 	}
+	
+	
+	
 	public Questions(String question) {
 		this.question = question; 
 	}
+	
+	
 
 	public Questions(int question_id, String question) {
 		this.question_id = question_id;
 		this.question = question; 
 	}
+	
+	
+	
+	
 
 	public Questions(String question, String question_id) {
 
 		this.setQuestion_id(question_id);
 		this.question = question;
 	}
+	
+	
+	
+	
+	
 
 	public int getQuestion_id() {
 		return question_id;
 	}
+	
+	
 
 	public void setQuestion_id(int question_id) {
 		this.question_id = question_id;
 	}
+	
+	
 	public void setQuestion_id(String question_id) {
 		try {
 			this.question_id = Integer.parseInt(question_id);
@@ -55,6 +73,7 @@ public class Questions {
 	public String getQuestion() {
 		return question;
 	}
+	
 
 	public void setQuestion(String question) {
 		this.question = question;
